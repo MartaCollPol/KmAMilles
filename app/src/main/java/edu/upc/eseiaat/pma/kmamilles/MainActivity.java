67bd618e96341ccdaf36eaf42f06a0697fffa3b8
Marta <maly-cat@hotmail.es>
2017-10-05 11:28:03 +0100

    Uploaded Project

---
+++ app/src/main/java/edu/upc/eseiaat/pma/kmamilles/MainActivity.java
@@ -0,0 +1,47 @@
+package edu.upc.eseiaat.pma.kmamilles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btn_millas = (Button) findViewById(R.id.btn_a_millas);
+        Button btn_km = (Button) findViewById(R.id.btn_a_km);
+        final EditText edit_km = (EditText) findViewById(R.id.edit_km);
+        final EditText edit_millas = (EditText) findViewById(R.id.edit_millas);
+        btn_km.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String smillas = edit_millas.getText().toString();
+                float millas = Float.parseFloat(smillas);
+                float km = millas * 1.609f;
+                String skm = String.format(Locale.getDefault(), "%f", km);
+                edit_km.setText(skm);
+
+            }
+        });
+        btn_millas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String skm = edit_km.getText().toString();
+                float km = Float.parseFloat(skm);
+                float millas = km / 1.609f;
+                String smillas = String.format(Locale.getDefault(),"%f",millas);
+                edit_millas.setText(smillas);
+
+            }
+        });
+    }
+}
+
